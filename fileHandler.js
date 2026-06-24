fe7478c81d1d90009b04655d54ba2c96bb476a4a
Maddie Ray <maddykray1611@gmail.com>
2026-06-24 01:45:12 -0500

    https://github.com/Maddy1611/-practice-importing-modules-Madhurima-R

---
+++ fileHandler.js
@@ -0,0 +1,28 @@
+
+
+/*Use the fs (File System) built-in module to define and export the
+following functions:*/
+
+const fs = require("fs");
+
+/*saveTasks(filePath, tasks): Writes the tasks to a file in JSON
+format.*/
+
+function saveTasks(filePath, tasks){
+fs.writeFileSync(filePath, JSON.stringify(tasks));
+console.log("Tasks saved!");
+}
+
+/*loadTasks(filePath): Reads and parses the tasks from the file.
+If the file doesn’t exist, return an empty array.*/
+
+function loadTasks(filePath) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+}
+
+module.exports = {saveTasks, loadTasks};
---
+++ taskManager.js
@@ -0,0 +1,19 @@
+
+// Define and export the following functions:
+
+
+//  addTask(tasks, task): Adds a new task to the task list.
+function addTask(tasks, task){
+tasks.push(task);
+console.log(`Task "${task}" added!`);
+}
+
+// listTasks(tasks): Logs all tasks to the console.
+function listTasks(tasks){
+    console.log(tasks);
+}
+
+module.exports = {
+    addTask,
+    listTasks
+};
